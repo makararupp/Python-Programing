b55da888a94c5e93ea02115894e53db37ef10072
makaraDevIt <makarasam68@gmail.com>
2026-09-09 11:59:32 +0700

    feat : implement string and dictionary

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+student ={'name':'Makara','age':23, 'major':'Computer Science'}
+# name:key, value:Makara
+# age:key, value:23
+# major:key, value:Computer Science
+print(student['name'])
+for key, value in student.items():
+    print(key, value)
---
+++ string_method.py
@@ -0,0 +1,5 @@
+message = 'hello python'
+print(message.lower())
+print(message.upper())
+print(message.title())
+print(message.replace('hello','hi'))
