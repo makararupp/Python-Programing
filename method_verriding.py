2288bbf02fb821926ec312c55e0d9fb205697904
makaraDevIt <makarasam68@gmail.com>
2026-09-09 19:29:02 +0700

    feat : implement polymorphism

---
+++ method_verriding.py
@@ -0,0 +1,13 @@
+class Animal:
+    name = ''
+
+    def eat(self):
+        print('I can eat')
+
+class Dog(Animal):
+
+    def eat(self):
+        print('I like to eat bones')
+
+labrador = Dog()
+labrador.eat();
---
+++ polymorphsip.py
@@ -0,0 +1,7 @@
+num1 = 1
+num2 = 2
+print(num1 + num2)
+
+str1 = 'Python'
+str2 = 'Programming'
+print(str1+" "+str2)
